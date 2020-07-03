56c43af9ce2e7c56925b49f2768a937f5936173b
xonix <xonixx@gmail.com>
2020-07-03 22:39:23 +0300

    simplify

--- prepare-for-qjs.py
+++ prepare-for-qjs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-s = open('modules/node_modules/jsqry/jsqry.js', 'r', encoding='UTF-8').read()
+s = open('node_modules/jsqry/jsqry.js', 'r', encoding='UTF-8').read()
 splitPart = '})(this, function (undefined) {'
 parts = s.split(splitPart)
 s = 'export default (function (root, factory) { return factory()\n' + splitPart + parts[1]
