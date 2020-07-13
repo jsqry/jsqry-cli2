61ac069f80acb06d8e010be917ae20cfc3d0dfea
xonix <xonixx@gmail.com>
2020-07-14 00:25:27 +0300

    Add colorized JSON output #4

--- colorJson.js
+++ colorJson.js
@@ -2,14 +2,14 @@
 
 // https://stackoverflow.com/a/28938235/104522
 const colors = {
-  // separator: "\x1b[1;37m",
   separator: "\x1b[1m",
   string: "\x1b[0;92m",
   number: "\x1b[0m",
-  boolean: "\x1b[1;37m",
+  boolean: "\x1b[1m",
   null: "\x1b[1;31m",
   key: "\x1b[34;1m",
 };
+const RESET = "\x1b[0m";
 
 export default function (jsonObj, spacing = 2) {
   const json = JSON.stringify(jsonObj, undefined, spacing);
@@ -20,14 +20,18 @@ export default function (jsonObj, spacing = 2) {
       function (match) {
         let colorCode = "number";
         if (/^"/.test(match)) {
-          if (/:$/.test(match)) colorCode = "key";
-          else colorCode = "string";
+          if (/:$/.test(match)) {
+            colorCode = "key";
+            match = match.substr(0, match.length - 1);
+          } else colorCode = "string";
         } else if (/true|false/.test(match)) colorCode = "boolean";
         else if (/null/.test(match)) colorCode = "null";
         const color = colors[colorCode] || "";
-        return `${color}${match}\x1b[0m${colors.separator}`;
+        return `${color}${match}${RESET}${colors.separator}${
+          colorCode === "key" ? ":" : ""
+        }`;
       }
     ) +
-    "\x1b[0m"
+    RESET
   );
 }
