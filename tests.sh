dbaaf181649ced6d32ecac09805b7b668813dc22
xonix <xonixx@gmail.com>
2020-10-21 03:32:51 +0300

    Automate tush installation #25

--- tests.sh
+++ tests.sh
@@ -4,7 +4,25 @@
 
 mydir=$(cd "$(dirname $0)" ; pwd)
 
-export PATH=$mydir/build:$PATH:~/proj_src/tush/bin/
+export PATH=$mydir/build:$PATH:$mydir/soft/tush/bin/
+
+cd "$mydir"
+
+if [ ! -d "./soft" ]
+then
+  mkdir "./soft"
+fi
+
+if [ ! -d "./soft/tush" ]
+then
+  echo
+  echo "Fetching tush..."
+  echo
+
+  cd "./soft"
+  git clone git@github.com:adolfopa/tush.git
+  cd "$mydir"
+fi
 
 #which jsqry
 #jsqry -v
