61dab7c02d2c22da55958f3cc0e212c8531b8d21
xonix <xonixx@gmail.com>
2020-10-21 16:42:45 +0300

    Automate installation and compilation of quickjs #26

--- build.sh
+++ build.sh
@@ -2,13 +2,45 @@
 
 set -e
 
-#QJS_HOME=~/proj_src/quickjs/
-#QJS_HOME=~/soft/quickjs-2020-07-05/
-QJS_HOME=~/soft/quickjs-2020-09-06/
+QJS_VERSION="2020-09-06"
 
-if [[ ! -d ./build ]]
-then
-    mkdir ./build
+mydir=$(
+  cd "$(dirname $0)"
+  pwd
+)
+
+cd "$mydir"
+
+QJS_HOME="$mydir/soft/quickjs-$QJS_VERSION"
+
+if [ ! -d "./soft" ]; then
+  mkdir "./soft"
+fi
+
+if [ ! -f "$QJS_HOME/qjsc" ]; then
+  echo
+  echo "Fetching QJS..."
+  echo
+
+  cd "./soft"
+
+  QJS=quickjs-$QJS_VERSION
+  wget https://bellard.org/quickjs/$QJS.tar.xz
+  tar xvf ./$QJS.tar.xz
+
+  echo
+  echo "Compile QJSC..."
+  echo
+
+  cd "./$QJS"
+
+  make qjsc libquickjs.lto.a
+fi
+
+cd "$mydir"
+
+if [[ ! -d ./build ]]; then
+  mkdir ./build
 fi
 
 cd ./build/
@@ -18,21 +50,22 @@ cd ./build/
 #    -fno-regexp \
 #    -fno-eval \
 
-echo "Compiling with $($QJS_HOME/qjsc | grep version)"
+echo
+echo "Compiling with $($QJS_HOME/qjsc | grep version)..."
+echo
 
 $QJS_HOME/qjsc \
-    -flto \
-    -fno-date \
-    -fno-proxy \
-    -fno-promise \
-    -fno-map \
-    -fno-typedarray \
-    -fno-string-normalize \
-    -fno-bigint \
-    ../jsqry-cli.js -o jsqry
+  -flto \
+  -fno-date \
+  -fno-proxy \
+  -fno-promise \
+  -fno-map \
+  -fno-typedarray \
+  -fno-string-normalize \
+  -fno-bigint \
+  ../jsqry-cli.js -o jsqry
 
 ls -lh jsqry
 
 cd ..
 ./tests.sh
-
