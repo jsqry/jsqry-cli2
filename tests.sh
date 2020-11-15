b87d11c5f5c6afdb952b17d030b40d7a5df94ff9
xonix <xonixx@gmail.com>
2020-11-15 22:33:29 +0200

    Make Github action just call build.sh #27

--- tests.sh
+++ tests.sh
@@ -2,29 +2,35 @@
 
 # Uses https://github.com/adolfopa/tush
 
-mydir=$(cd "$(dirname $0)" ; pwd)
+mydir=$(
+  cd "$(dirname $0)"
+  pwd
+)
 
 export PATH=$mydir/build:$PATH:$mydir/soft/tush/bin/
 
 cd "$mydir"
 
-if [ ! -d "./soft" ]
-then
+if [ ! -d "./soft" ]; then
   mkdir "./soft"
 fi
 
-if [ ! -d "./soft/tush" ]
-then
+if [ ! -d "./soft/tush" ]; then
   echo
   echo "Fetching tush..."
   echo
 
   cd "./soft"
-  git clone git@github.com:adolfopa/tush.git
+  wget https://github.com/adolfopa/tush/archive/master.zip -O./tush.zip
+  unzip ./tush.zip
+  mv tush-master tush
   cd "$mydir"
 fi
 
 #which jsqry
 #jsqry -v
 
-tush-check tests.tush && echo 'TESTS PASSED' || (echo '!!! TESTS FAILED !!!' ; exit 1)
+tush-check tests.tush && echo 'TESTS PASSED' || (
+  echo '!!! TESTS FAILED !!!'
+  exit 1
+)
